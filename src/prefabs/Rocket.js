f371422ffad7fd02756474882d07354d751bf3d8
Qingzhao Cao <qcao5@ucsc.edu>
2021-04-13 04:11:14 +0800

    make shot faster

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -8,34 +8,34 @@ class Rocket extends Phaser.GameObjects.Sprite {
         this.moveSpeed = 5;         // pixels per frame
         this.sfxRocket = scene.sound.add('sfx_rocket')  // add rocket sfx
     }
-}
 
-update() {
-    // left/right movement
-    if(!this.isFiring) {
-        if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
-            this.x -= this.moveSpeed;
-        } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
-            this.x += this.moveSpeed;
+    update() {
+        // left/right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed;
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed;
+            }
+        }
+        // fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+            this.isFiring = true;
+            this.sfxRocket.play();
+        }
+        // if fired, move up
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+        // reset on miss
+        if(this.y <= borderUISize * 3 + borderPadding) {
+            this.reset();
         }
     }
-    // fire button
-    if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
-        this.isFiring = true;
-        this.sfxRocket.play();
-    }
-    // if fired, move up
-    if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
-        this.y -= this.moveSpeed;
-    }
-    // reset on miss
-    if(this.y <= borderUISize * 3 + borderPadding) {
-        this.reset();
-    }
 
-// reset rocket to "ground"
-reset() {
-    this.isFiring = false;
-    this.y = game.config.height - borderUISize - borderPadding;
-   }
+    // reset rocket to "ground"
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+    }
 }
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -19,18 +19,18 @@ class Menu extends Phaser.Scene {
           color: '#843605',
           align: 'right',
           padding: {
-              top: 5,
-              bottom: 5,
+              top: 8,
+              bottom: 8,
           },
           fixedWidth: 0
       }
       
       // show menu text
       this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-      this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+      this.add.text(game.config.width/2, game.config.height/2, 'Use [←][→]arrows to move & [F] to fire', menuConfig).setOrigin(0.5);
       menuConfig.backgroundColor = '#00FF00';
       menuConfig.color = '#000';
-      this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+      this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press [←] for Novice or [→] for Expert', menuConfig).setOrigin(0.5);
 
       // define keys
       keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
